60d3dfde4a6174c83dd6dfd11593bed5dff4cc9a
Mark Hernández <herzmark41220@gmail.com>
2021-02-12 14:40:54 -0600

    Agregar archivos

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+password= "cocusmaximus"
+print ("Ingrese la contraseña")
+a = str(input())
+if password.lower() == a.lower():
+    print("la contraseña es correcta")
+else:
+    print("la contraeseña es incorrecta")
